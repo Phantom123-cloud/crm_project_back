2577600210ee5940a953e17c9b0d47853ab8a254
IhorSliepchenko1 <igorslepchenko.cc@gmail.com>
2026-01-12 13:10:51 +0200

    fix len add prod

--- src/products/dto/create-products.dto.ts
+++ src/products/dto/create-products.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class CreateProductsDto {
   @IsString({ message: 'Поле должно быть строкой' })
-  @Length(5, 120, { message: 'Длина от 5 до 120 символов' })
+  @Length(5, 150, { message: 'Длина от 5 до 150 символов' })
   @IsNotEmpty({ message: 'Обязательное поле' })
   name: string;
 }
